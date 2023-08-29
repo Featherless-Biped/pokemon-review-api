c04d719f27a0320d16ebf43b280ceb55d198c9e3
Featherless-Biped <114402536+Featherless-Biped@users.noreply.github.com>
2023-08-29 16:43:31 +0300

    Unit Testing for Web APIs Controllers

--- PokemonReviewApp.Tests/UnitTest1.cs
+++ PokemonReviewApp.Tests/UnitTest1.cs
@@ -1,13 +1,10 @@
-using Xunit;
+namespace PokemonReviewApp.Tests;
 
-namespace PokemonReviewApp.Tests
+public class UnitTest1
 {
-    public class UnitTest1
+    [Fact]
+    public void Test1()
     {
-        [Fact]
-        public void Test1()
-        {
 
-        }
     }
 }
